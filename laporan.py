08cbef08a2fa5b1ce4d5e055138e330a370c018a
glennkevin62827383-spec <glennkevin62827383@gmail.com>
2026-09-22 16:14:15 +0000

    Mengubah judul laporan oleh Anggota B

--- laporan.py
+++ laporan.py
@@ -1,4 +1,4 @@
 def tampilkan_laporan(data):
-    print("\n===== LAPORAN HASIL PANEN =====")
+    print("\n===== LAPORAN HASIL PANEN KELOMPOK B =====")
     print(f"Komoditas : {data['komoditas']}")
     print(f"Jumlah    : {data['jumlah']} kg")
