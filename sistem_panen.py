ab4127747c1f8037ddad7fa3baf7347e6fc84df2
glennlizcel89-afk <glennlizcel89@gmail.com>
2026-09-22 16:24:21 +0000

    Mengubah pesan input dan output di main

--- sistem_panen.py
+++ sistem_panen.py
@@ -1,6 +1,6 @@
 
 def input_data_panen():
-    komoditas = input("Masukkan nama komoditas: ")
+    komoditas = input("Masukkan nama komoditas panen: ") # Perubahan di branch main
     jumlah = float(input("Masukkan jumlah hasil panen (kg): "))
 
     return {
@@ -12,6 +12,6 @@ def input_data_panen():
 if __name__ == "__main__":
     data = input_data_panen()
 
-    print("\nData hasil panen:")
+    print("\nData hasil panen main:") # Perubahan di branch main
     print(f"Komoditas : {data['komoditas']}")
     print(f"Jumlah    : {data['jumlah']} kg")
