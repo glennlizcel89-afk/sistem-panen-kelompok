d1b70d3b7f54ebcc6a5930ca0c0d3009f6eddfe8
glennlizcel89-afk <glennlizcel89@gmail.com>
2026-09-22 15:51:50 +0000

    Menambahkan fungsi input data panen

--- sistem_panen.py
+++ sistem_panen.py
@@ -1,6 +1,17 @@
 
-def main():
-    print("Sistem Pengelolaan Hasil Panen")
-    
+def input_data_panen():
+    komoditas = input("Masukkan nama komoditas: ")
+    jumlah = float(input("Masukkan jumlah hasil panen (kg): "))
+
+    return {
+        "komoditas": komoditas,
+        "jumlah": jumlah
+    }
+
+
 if __name__ == "__main__":
-    main()
+    data = input_data_panen()
+
+    print("\nData hasil panen:")
+    print(f"Komoditas : {data['komoditas']}")
+    print(f"Jumlah    : {data['jumlah']} kg")
