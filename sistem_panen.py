2153a47a347528f49329041d6141caef3e3c23d8
glennlizcel89-afk <glennlizcel89@gmail.com>
2026-09-22 16:24:31 +0000

    Mengubah pesan input dan output di fitur-input

--- sistem_panen.py
+++ sistem_panen.py
@@ -1,7 +1,7 @@
 
 def input_data_panen():
-    komoditas = input("Masukkan nama komoditas: ")
-    jumlah = float(input("Masukkan jumlah hasil panen (kg): "))
+    komoditas = input("Masukkan jenis komoditas: ") # Perubahan di branch fitur-input
+    jumlah = float(input("Masukkan jumlah hasil panen (kilogram): ")) # Perubahan di branch fitur-input
 
     return {
         "komoditas": komoditas,
@@ -12,6 +12,6 @@ def input_data_panen():
 if __name__ == "__main__":
     data = input_data_panen()
 
-    print("\nData hasil panen:")
-    print(f"Komoditas : {data['komoditas']}")
-    print(f"Jumlah    : {data['jumlah']} kg")
+    print("\nData panen fitur-input:") # Perubahan di branch fitur-input
+    print(f"Jenis Komoditas : {data['komoditas']}")
+    print(f"Jumlah Panen    : {data['jumlah']} kilogram")
